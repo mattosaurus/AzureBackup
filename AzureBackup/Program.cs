5b787c84c6b8a4fbdc0d9e06c0b4a94970c2fcdd
mattosaurus <mattosaurus@outlook.com>
2017-10-08 17:09:03 +0100

    Creation.

--- AzureBackup/Program.cs
+++ AzureBackup/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace AzureBackup
 {
@@ -6,7 +13,57 @@ namespace AzureBackup
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Configuration
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            string backupPath = args[0];
+
+            if (!backupPath.EndsWith("\\"))
+            {
+                backupPath += "\\";
+            }
+
+            // Check directory exists
+            if (!Directory.Exists(backupPath))
+            {
+                throw new ArgumentException("Specified directory doesn't exist.");
+            }
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(backupPath);
+
+            string[] filePaths = Directory.GetFiles(backupPath, "*", SearchOption.AllDirectories);
+
+            List<FileInfo> files = filePaths.Select(x => new FileInfo(x)).ToList();
+
+            // Retrieve storage account from connection string.
+            string containerConnectionString = configuration["Azure:ContainerConnectionString"];
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(containerConnectionString);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve a reference to a container.
+            CloudBlobContainer container = blobClient.GetContainerReference(configuration["Azure:ContainerName"]);
+
+            // Upload files
+            foreach (FileInfo file in files)
+            {
+                string blobPath = file.FullName.Replace(backupPath, "");
+                blobPath = directoryInfo.Name + "\\" + blobPath;
+
+                // Retrieve reference to a blob
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference(file.Name);
+
+                // Create or overwrite the "myblob" blob with contents from a local file.
+                using (FileStream fileStream = File.OpenRead(file.FullName))
+                {
+                    blockBlob.UploadFromStreamAsync(fileStream);
+                }
+            }
         }
     }
 }
